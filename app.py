5ed96925bc97d64e4dfd753990dea3ccfb20dae3
Dylan Lewis <dylan.lewis@post.runi.ac.il>
2024-06-03 13:19:42 +0300

    adding app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+# Import necessary libraries
+from flask import Flask, render_template, request, redirect, jsonify
+from cls import classify
+import os
+import time
+
+
+# Create a Flask web application
+app = Flask(__name__)
+
+# Function to get a response from the chatbot
+def get_response(userText):
+    return "response!"
+
+# Define app routes
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/get")
+# Function for the bot response
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(get_response(userText))
+
+@app.route('/getMessageDetails', methods=['POST'])
+def get_message_details():
+    data = request.get_json()
+    message = data.get('msg', '')
+    cost = message
+    categorie = classify(message)
+    return jsonify({'categorie': categorie, 'cost': cost})
+
+@app.route('/refresh')
+def refresh():
+    time.sleep(600) # Wait for 10 minutes
+    return redirect('/refresh')
+
+# Run the Flask app
+if __name__ == "__main__":
+    app.run()
